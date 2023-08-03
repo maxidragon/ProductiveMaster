c06d4a6ddced5404ac213941e7a3ae7e9ce3a535
maxidragon <mgwspinanie2006@wp.pl>
2023-08-03 12:57:31 +0200

    Add change password tests

--- backend/auth/tests.py
+++ backend/auth/tests.py
@@ -49,3 +49,32 @@ class ListCreateUserTests(TestCase):
             url, data, format='json', HTTP_AUTHORIZATION=f'Token {token}')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(User.objects.count(), 3)
+
+class ChangePasswordTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='user', password='normalpassword')
+    
+    def authenticate(self, username, password):
+        response = self.client.post(reverse(
+            'get-token'), {'username': username, 'password': password}, format='json')
+        token = response.data['token']
+        return token
+    
+    def test_change_password(self):
+        url = reverse('auth-password-change')
+        token = self.authenticate('user', 'normalpassword')
+        response = self.client.post(
+            url, {'old_password': 'normalpassword', 'new_password': 'MyTree123'}, format='json', HTTP_AUTHORIZATION=f'Token {token}')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.user.refresh_from_db()
+        self.assertTrue(self.user.check_password('MyTree123'))
+    
+    def test_change_password_wrong_old_password(self):
+        url = reverse('auth-password-change')
+        token = self.authenticate('user', 'normalpassword')
+        response = self.client.post(
+            url, {'old_password': '123', 'new_password': 'MyTree123'}, format='json', HTTP_AUTHORIZATION=f'Token {token}')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        
