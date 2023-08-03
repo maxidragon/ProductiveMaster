f90968eb5bd155789326a00a84a81f57fce2e44d
maxidragon <mgwspinanie2006@wp.pl>
2023-08-03 18:53:27 +0200

    Add GoalCategoryDetail tests

--- backend/goals/tests.py
+++ backend/goals/tests.py
@@ -116,3 +116,51 @@ class GoalDetailTest(TestCase):
         token = self.authenticate('nonowner', 'nonownerpassword')
         response = self.client.delete(url, HTTP_AUTHORIZATION=f'Token {token}')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+class GoalCategoryDetail(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser', password='testpassword')
+        self.user2 = User.objects.create_user(
+            username='nonowner', password='nonownerpassword')
+        self.goal_category = GoalCategory.objects.create(
+            title='Test Goal Category', owner=self.user)
+        
+    def authenticate(self, username, password):
+        response = self.client.post(reverse(
+            'get-token'), {'username': username, 'password': password}, format='json')
+        token = response.data['token']
+        return token
+    
+    def test_get_goal_category(self):
+        url = reverse('goal-category-detail', kwargs={'pk': self.goal_category.id})
+        token = self.authenticate('testuser', 'testpassword')
+        response = self.client.get(url, HTTP_AUTHORIZATION=f'Token {token}')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], self.goal_category.title)
+        
+    def test_update_goal_category(self):
+        url = reverse('goal-category-detail', kwargs={'pk': self.goal_category.id})
+        token = self.authenticate('testuser', 'testpassword')
+        response = self.client.put(url, {'title': 'Updated goal category'}, HTTP_AUTHORIZATION=f'Token {token}', content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], 'Updated goal category') 
+        
+    def test_update_goal_category_forbidden(self):
+        url = reverse('goal-category-detail', kwargs={'pk': self.goal_category.id})
+        token = self.authenticate('nonowner', 'nonownerpassword')
+        response = self.client.put(url, {'title': 'Updated goal category'}, HTTP_AUTHORIZATION=f'Token {token}', content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_delete_goal_category(self):
+        url = reverse('goal-category-detail', kwargs={'pk': self.goal_category.id})
+        token = self.authenticate('testuser', 'testpassword')
+        response = self.client.delete(url, HTTP_AUTHORIZATION=f'Token {token}')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        
+    def test_delete_goal_category_forbidden(self):
+        url = reverse('goal-category-detail', kwargs={'pk': self.goal_category.id})
+        token = self.authenticate('nonowner', 'nonownerpassword')
+        response = self.client.delete(url, HTTP_AUTHORIZATION=f'Token {token}')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
