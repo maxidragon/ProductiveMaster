c7113c26ef5b52b747ddb0bd7d37df480ff25111
maxidragon <mgwspinanie2006@wp.pl>
2023-08-04 10:46:55 +0200

    Update API url

--- backend/productivemaster/urls.py
+++ backend/productivemaster/urls.py
@@ -1,13 +1,26 @@
+import os
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('get-token/', views.obtain_auth_token, name='get-token'),
-    path('auth/', include('auth.urls')),
-    path('notes/', include('notes.urls')),
-    path('activities/', include('activities.urls')),
-    path('goals/', include('goals.urls')),
-    path('', include('tasks.urls')),
-]
+if os.environ.get('DEBUG') == 'True':
+    urlpatterns = [
+        path('admin/', admin.site.urls),
+        path('get-token/', views.obtain_auth_token, name='get-token'),
+        path('auth/', include('auth.urls')),
+        path('notes/', include('notes.urls')),
+        path('activities/', include('activities.urls')),
+        path('goals/', include('goals.urls')),
+        path('', include('tasks.urls')),
+    ]
+    
+else:
+    urlpatterns = [
+        path('admin/', admin.site.urls),
+        path('api/get-token/', views.obtain_auth_token, name='get-token'),
+        path('api/auth/', include('auth.urls')),
+        path('api/notes/', include('notes.urls')),
+        path('api/activities/', include('activities.urls')),
+        path('api/goals/', include('goals.urls')),
+        path('api/', include('tasks.urls')),
+    ]    
--- frontend/src/logic/request.ts
+++ frontend/src/logic/request.ts
@@ -1,11 +1,12 @@
 export const backendRequest = (path: string, method: string, useAuth: boolean, body?: Object) => {
+    const apiUrl = process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:8000';
     const token = localStorage.getItem("token");
     const headers = new Headers();
     headers.append("Content-Type", "application/json");
     if (token && useAuth) {
         headers.append("Authorization", `Token ${token}`);
     }
-    return fetch(`http://localhost:8000/${path}`, {
+    return fetch(`${apiUrl}/${path}`, {
         method: method,
         headers: headers,
         redirect: "follow",
