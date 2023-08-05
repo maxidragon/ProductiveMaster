839dbc89cd4f637374cfdfed387fc8fc04ee2171
maxidragon <mgwspinanie2006@wp.pl>
2023-08-05 20:15:24 +0200

    Fix tests

--- backend/productivemaster/settings.py
+++ backend/productivemaster/settings.py
@@ -96,8 +96,14 @@ WSGI_APPLICATION = 'productivemaster.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
-if DEBUG == False:
+if not DEBUG:
 		DATABASES = {
 		    'default': {
 		        'ENGINE': 'django.db.backends.postgresql',
@@ -108,13 +114,7 @@ if DEBUG == False:
 		        'PORT': os.environ.get('PGPORT'),
 		    }
 		}
-else:
-    DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
