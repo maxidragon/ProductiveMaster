d737575923d977402156ea57adfd72991deba376
maxidragon <mgwspinanie2006@wp.pl>
2023-08-14 14:55:42 +0200

    Add display timezones

---
+++ frontend/src/Components/Table/TimezonesTable.tsx
@@ -0,0 +1,28 @@
+import { TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody } from "@mui/material";
+import { Timezone } from "../../logic/interfaces";
+import TimezoneRow from "./Row/TimezoneRow";
+
+const TimezonesTable = (props: {
+    timezones: Timezone[];
+}) => {
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }}>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Name</TableCell>
+                        <TableCell>Time</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {props.timezones.map((timezone: Timezone) => (
+                        <TimezoneRow key={timezone.id} timezone={timezone} />
+                    ))}
+
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+};
+
+export default TimezonesTable;
--- frontend/src/logic/interfaces.ts
+++ frontend/src/logic/interfaces.ts
@@ -1,62 +1,68 @@
 export interface Note {
-    id: number;
-    title: string;
-    description: string;
-};
+  id: number;
+  title: string;
+  description: string;
+}
 
 export interface Goal {
-    id: number;
-    title: string;
-    description: string;
-    deadline: Date;
-    is_achieved: boolean;
-    goal_category?: GoalCategory;
-};
+  id: number;
+  title: string;
+  description: string;
+  deadline: Date;
+  is_achieved: boolean;
+  goal_category?: GoalCategory;
+}
 
 export interface Project {
-    id: number;
-    title: string;
-    description: string;
-    status: string;
-    github?: string;
-    num_tasks_todo?: number;
-    num_tasks_in_progress?: number;
-    num_tasks_done?: number;
-};
+  id: number;
+  title: string;
+  description: string;
+  status: string;
+  github?: string;
+  num_tasks_todo?: number;
+  num_tasks_in_progress?: number;
+  num_tasks_done?: number;
+}
 
 export interface Task {
-    id: number;
-    title: string;
-    description?: string;
-    status: string;
-    issue?: string;
-    pull_request?: string;
-    project: Project;
-};
+  id: number;
+  title: string;
+  description?: string;
+  status: string;
+  issue?: string;
+  pull_request?: string;
+  project: Project;
+}
 
 export interface Activity {
-    id: number;
-    title: string;
-    description: string;
-    start_time: Date;
-    end_time: Date;
-};
+  id: number;
+  title: string;
+  description: string;
+  start_time: Date;
+  end_time: Date;
+}
 
 export interface GoalCategory {
-    id: number;
-    title: string;
-};
+  id: number;
+  title: string;
+}
 
 export interface UserSettings {
-    id: number;
-    email: string;
-    username: string;
-};
+  id: number;
+  email: string;
+  username: string;
+}
 
 export interface UserData {
-    github_profile?: string;
-    wakatime_api_key?: string;
-    gprm_stats?: string;
-    gprm_streak?: string;
-    gprm_languages?: string;
-};
+  github_profile?: string;
+  wakatime_api_key?: string;
+  gprm_stats?: string;
+  gprm_streak?: string;
+  gprm_languages?: string;
+}
+
+export interface Timezone {
+  id: number;
+  name: string;
+  display_name: string;
+}
