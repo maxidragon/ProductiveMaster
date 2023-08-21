5863da5e2923496918d85f4429b75526adebfadc
maxidragon <mgwspinanie2006@wp.pl>
2023-08-21 10:06:24 +0200

    Fix console errors

--- frontend/src/Components/TabPanel.tsx
+++ frontend/src/Components/TabPanel.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -17,11 +17,7 @@ const TabPanel = (props: TabPanelProps) => {
       aria-labelledby={`tab-${index}`}
       {...other}
     >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
     </div>
   );
 };
