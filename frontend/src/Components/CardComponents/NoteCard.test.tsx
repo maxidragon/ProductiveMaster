e1d501043666c1e3a28694094ef34c58873f08d8
maxidragon <maksymilian.gala1@gmail.com>
2023-12-25 21:20:37 +0100

    Fix notecard tests

--- frontend/src/Components/CardComponents/NoteCard.test.tsx
+++ frontend/src/Components/CardComponents/NoteCard.test.tsx
@@ -6,7 +6,13 @@ describe("NoteCard component", () => {
   const mockNote = {
     id: 1,
     title: "Test Note",
-    description: "This is a test note description.",
+    description: "This is a test note description over 30 characters long.",
+  };
+
+  const mockNote2 = {
+    id: 2,
+    title: "Test Note 2",
+    description: "Under 30 characters long.",
   };
 
   it("should render NoteCard correctly with note data", () => {
@@ -18,8 +24,20 @@ describe("NoteCard component", () => {
     const titleElement = screen.getByText(mockNote.title);
     expect(titleElement).toBeInTheDocument();
     const descriptionElement = screen.getByText(
-      mockNote.description.substring(0, 200) + "...",
+      mockNote.description.substring(0, 30) + "...",
     );
+    expect(descriptionElement).toBeInTheDocument();
+  });
+
+  it("should render NoteCard correctly with note data under 30 characters", () => {
+    render(
+      <MemoryRouter>
+        <NoteCard note={mockNote2} />
+      </MemoryRouter>,
+    );
+    const titleElement = screen.getByText(mockNote2.title);
+    expect(titleElement).toBeInTheDocument();
+    const descriptionElement = screen.getByText(mockNote2.description);
     expect(descriptionElement).toBeInTheDocument();
   });
 });
