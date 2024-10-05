45e20140c90d7e6588708463535c11487a964f2b
Maksymilian Gala <maksymilian.gala1@gmail.com>
2024-10-05 09:50:36 +0200

    fix(backend): cronjob

--- backend/daily_tasks/cron.py
+++ backend/daily_tasks/cron.py
@@ -3,13 +3,11 @@ from datetime import datetime, timedelta
 
 def move_uncompleted_tasks_to_the_next_day():
     print('Moving uncompleted tasks to the next day')
-    #yesterday = datetime.now().date() - timedelta(days=1)
-    yesterday = datetime.now().date().strftime("%Y-%m-%d")
+    yesterday = datetime.now().date() - timedelta(days=1)
     date_obj = datetime.strptime(yesterday, "%Y-%m-%d")
     uncompleted_tasks = DailyTask.objects.filter(
             date__year=date_obj.year, date__month=date_obj.month, date__day=date_obj.day, completed_at=None)
     for task in uncompleted_tasks:
-        #task.date = datetime.now().date()
-        task.date = datetime.now().date() + timedelta(days=1)
+        task.date = datetime.now().date()
         task.save()
     
